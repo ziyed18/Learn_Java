32aa65279c7274cf0e9dd78bde71770e2271ba50
Ziyed | زيـاد <87272723+ziyed18@users.noreply.github.com>
2025-11-19 18:27:05 +0100

    Add files via upload

---
+++ Basics/if_else_elif.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class case {
+    public static void main(String[] args) {
+        float moy;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Moyenne:? ");
+        M=clavier.NextFloat();
+        if(0<=moy && moy<10){
+            System
+        }
+    }
+}
