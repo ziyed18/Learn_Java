418ea99e9faa0b84308d47575ea5d7fd5dfb7f2e
Ziyed | زيـاد <87272723+ziyed18@users.noreply.github.com>
2025-11-20 23:08:27 +0100

    Add files via upload

---
+++ Basics/Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator{
+	public static void main(String[]args){
+		int a=10; //double reel exemple: double a=10.5 
+		int b=3;
+		System.out.println("a + b = "+(a+b));
+		System.out.println("a - b = "+(a-b));
+		System.out.println("a * b = "+(a*b));
+		System.out.println("a / b = "+(a/b)); // /
+		System.out.println("a % b = "+(a%b)); //REST
+		
+	}
+}
---
+++ Basics/Compare.java
@@ -0,0 +1,13 @@
+public class Compare{
+	public static void main(String[] args){
+		int num1=8;
+		int num2=12;
+		if (num1>num2){
+			System.out.println("Le premier nombre est plus grand");
+		}else if(num1<num2){
+			System.out.println("Le deuxieme nombre est plus grand");
+		}else{
+			System.out.println("Les deux nombres sont egaux");
+		}
+	}
+}
